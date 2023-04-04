8266c0b8a4039abd2444cb7aba249733022c9746
Tommy Zimmerman <tomzimmerman7@protonmail.com>
2023-04-04 13:05:05 -0400

    completed template for assignment

---
+++ problem.js
@@ -0,0 +1,22 @@
+/*
+    For this practice project I would like you to to program the following in javascript:
+
+    1.) Create a function that returns a random value between 0 and 255 **Hint: this can be done by using the following ===>>> Math.floor(Math.random() * 255);
+
+    2) Create another function that uses the random number generator to generate a new random color with **Re-watch https://teamtreehouse.com/library/javascript-loops/the-refactor-challenge-duplicate-code to see how you can make an rgb string with a random number generator.
+
+    3) Use query selector to grab the div with the class "container-fluid"
+
+    4) use query selector to grab the button
+
+    5) Add an event listener to the button. The event listener should change the background-color CSS property on the container fluid div to the new randomly generated color every time the button is clicked.
+
+    Here are additional resources in order to complete this task:
+
+    How to write a function: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Functions
+    How to use querySelector(): https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelector
+    How to uses eventListener(): https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
+    How to change a CSS Property using DOM manipulation: https://www.w3schools.com/js/js_htmldom_css.asp
+
+
+*/
